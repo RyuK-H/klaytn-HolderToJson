1981471a3968fad99f852cca500a62b60bce9d66
RyuK-H <ryugihyeok@gmail.com>
2021-01-04 20:57:57 +0900

    ENH: JSON Prettier 설정 / README.MD추가

---
+++ README.md
@@ -0,0 +1,13 @@
+### VSCode Setting (workspace)
+```
+{
+  "editor.formatOnSave": true,
+  "prettier.eslintIntegration": true,
+  "files.autoSave": "onFocusChange",
+  "json.format.enable": false
+}
+```
+
+### RUN
+(1) Get Holder `npm run dev-get`
+(2) Airdrop `npm run dev-airdrop`
--- src/GetHolder.js
+++ src/GetHolder.js
@@ -5,7 +5,9 @@ const MAX_PAGE = 1;
 
 const getHolders = async (page) => {
   try {
-    return await axios.get(`https://api-cypress.scope.klaytn.com/v1/tokens/0x5288f80f4145035866ac4cb45a4d8dea889ec827/holders?page=${page}`);
+    return await axios.get(
+      `https://api-cypress.scope.klaytn.com/v1/tokens/0x5288f80f4145035866ac4cb45a4d8dea889ec827/holders?page=${page}`,
+    );
   } catch (error) {
     console.error(error);
   }
@@ -15,33 +17,33 @@ const holdersToJSON = async (page) => {
   const holders = await getHolders(page);
 
   if (holders.data) {
-    for(let i = 0; i < holders.data.result.length; i++) {
+    for (let i = 0; i < holders.data.result.length; i++) {
       const temp = {
         address: holders.data.result[i].address,
         amount: holders.data.result[i].amountHeld,
-      }
+      };
       holdersData.push(temp);
     }
   }
 };
 
 const loopGETHolder = async () => {
-  for(let i = 1; i <= MAX_PAGE; i++) {
+  for (let i = 1; i <= MAX_PAGE; i++) {
     await holdersToJSON(i);
   }
-  
+
   await writeJSON();
-}
+};
 
 const writeJSON = async () => {
   const data = JSON.stringify(holdersData);
 
   fs.writeFile('holder.json', data, (err) => {
     if (err) {
-        throw err;
+      throw err;
     }
-    console.log("JSON data is saved.");
+    console.log('JSON data is saved.');
   });
-}
+};
 
 loopGETHolder();
