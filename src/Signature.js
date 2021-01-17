752693e6606371db4ef8425fc8585f22c79a66ca
RyuK-H <ryugihyeok@gmail.com>
2021-01-18 06:46:07 +0900

    CLN: 미사용 스크립트 삭제

--- src/Signature.js
+++
@@ -1,9 +0,0 @@
-const Caver = require('caver-js');
-const caver = new Caver();
-const Config = require('../config.js');
-
-const CONTRACT_ADDRESS = Config.TOKENADDRESS;
-const PRIVATE_KEY = Config.PRKEY;
-const SIG = caver.klay.accounts.sign(CONTRACT_ADDRESS, PRIVATE_KEY)
-
-console.log(SIG)
