9466d03002e6292fdcd1ca4bd2635f6b178ec54b
RyuK-H <ryugihyeok@gmail.com>
2021-01-19 02:58:17 +0900

    ENH: 스냅샷 1차

--- src/caver/verifySnapShot.js
+++ src/caver/verifySnapShot.js
@@ -7,21 +7,40 @@ const verifySnapShot = async () => {
   let totalAmount = 0;
   let totalUser = 0;
 
-  for (let i = 0; i <= HOLDER_GROUP; i++) {
-    const holderGroup = await require(`../../data/balance/${i}_balance.json`);
-    totalUser += holderGroup.length;
+  // for (let i = 0; i <= HOLDER_GROUP; i++) {
+  //   const holderGroup = await require(`../../data/balance/${i}_balance.json`);
+  //   totalUser += holderGroup.length;
 
-    holderGroup.forEach((data) => {
-      if (data.balance === 0) {
-        console.log('?');
-      }
-      const BNBalance = caver.utils.toBN(data.balance);
-      totalAmount = caver.utils.toBN(totalAmount).add(BNBalance).toString();
-    });
-  }
+  //   console.log(`${i} Page User Count : ${holderGroup.length}`);
+
+  //   holderGroup.forEach((data) => {
+  //     if (data.balance === 0) {
+  //       console.log('?');
+  //     }
+
+  //     if (data.address.match(/0x6a538e3f884f396586f8876424c320424a11df59/i)) {
+  //       console.log(data.balance);
+  //     }
+
+  //     const BNBalance = caver.utils.toBN(data.balance);
+  //     totalAmount = caver.utils.toBN(totalAmount).add(BNBalance).toString();
+  //   });
+  // }
+
+  // console.log(`Big Number Total : ${totalAmount}`);
+  // console.log(`Total : ${caver.utils.convertFromPeb(totalAmount, 'KLAY')}`);
+  // console.log(`Total User : ${totalUser}`);
+
+  const holder = await require(`../../data/balance_holders.json`);
+
+  holder.forEach((data) => {
+    const BNBalance = caver.utils.toBN(data.balance);
+    totalAmount = caver.utils.toBN(totalAmount).add(BNBalance).toString();
+  });
+
   console.log(`Big Number Total : ${totalAmount}`);
   console.log(`Total : ${caver.utils.convertFromPeb(totalAmount, 'KLAY')}`);
-  console.log(`Total User : ${totalUser}`);
+  console.log(`Total User : ${holder.length}`);
 };
 
 module.exports = verifySnapShot;
--- src/index.js
+++ src/index.js
@@ -1,5 +1,4 @@
 const getHolders = require('./caver/getHolders');
-const kasTest = require('./caver/kasTest');
 const integrated = require('./caver/deduplication');
 const getUsersInfo = require('./caver/getBalance');
 const verifySnapShot = require('./caver/verifySnapShot');
@@ -8,7 +7,6 @@ const holderList = require('../data/1_11_holders.json');
 const result = require('../data/balance_holders.json');
 
 // getHolders();
-// kasTest();
 // integrated();
 // getUsersInfo();
 verifySnapShot();
