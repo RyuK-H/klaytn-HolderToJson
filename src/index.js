109a06abff8e80007df0459c23a20e33e79054e6
RyuK-H <ryugihyeok@gmail.com>
2021-01-19 20:07:46 +0900

    ENH: scope, caver holder 비교

--- src/index.js
+++ src/index.js
@@ -1,16 +1,30 @@
-const getHolders = require('./caver/getHolders');
-const integrated = require('./caver/deduplication');
-const getUsersInfo = require('./caver/getBalance');
-const divisionHolders = require('./caver/divisionHolders');
-const verifySnapShot = require('./caver/verifySnapShot');
-const multiTransfer = require('./caver/multiTransfer');
-const caverHolders = require('./data/balance_holders.json');
+const fs = require('fs');
+const caverHolders = require('../data/result/caver_holders.json');
+const scopeHolders = require('../data/result/scope_holders.json');
 
-// getHolders();
-// integrated();
-// getUsersInfo();
-// divisionHolders();
-// verifySnapShot();
-// multiTransfer();
+const result = [];
 
-console.log(caverHolders.length);
+const writeJSON = async () => {
+  const data = JSON.stringify(result);
+
+  fs.writeFile(`./data/result/missing_holders.json`, data, (err) => {
+    if (err) {
+      throw err;
+    }
+    console.log('missing_holders.json data is saved.');
+  });
+};
+
+for (let i = 0; i < caverHolders.length; i++) {
+  console.log(`${i}번째 진입`);
+  for (let j = 0; j < scopeHolders.length; j++) {
+    if (caverHolders[i].address.match(new RegExp(scopeHolders[j].address, 'i'))) {
+      console.log(`${j}번째 탈출`);
+      break;
+    } else if (j === scopeHolders.length - 1) {
+      console.log(caverHolders[i].address);
+      result.push(caverHolders[i].address);
+    }
+  }
+}
+writeJSON();
